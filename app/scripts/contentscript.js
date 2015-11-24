c495b3104a9e3e0402e77981396640ded18412d8
Vladislav <ruspython@gmail.com>
2015-11-24 18:22:00 +0300

    Facebook skeleton

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -3,13 +3,12 @@ ChromeBuffer = (function (w) {
     var $parent,
         $modal,
         $shadow,
-        $messageField,
+        $commentField,
         $submitBtn,
-        sharedData = {
-            title: '',
-            text: '',
-            imageSrc: ''
-        }
+        $imageBox,
+        $titleField,
+        $textField,
+        sharedData = {}
         ;
     return {
 
@@ -17,37 +16,67 @@ ChromeBuffer = (function (w) {
             $parent = $('<div class="buffer-overlay" style="display: none"></div>');
             $modal = $('<div class="modal"></div>');
             $shadow = $('<div class="shadow"></div>');
-            $messageField = $('<textarea></textarea>');
+            $commentField = $('<textarea></textarea>');
             $submitBtn = $('<button type="submit">Share</button>');
+
+            $imageBox = $('<img class="thumbnail"/>');
+            $titleField = $('<h3 class="title"></h3>');
+            $textField = $('<div class="text-content"></div>');
 
 
             $parent.appendTo($('body'));
             $parent.append($shadow);
             $parent.append($modal);
-            $modal.append($messageField);
+            $modal.append($commentField);
             $modal.append($submitBtn);
 
-            $submitBtn.click(onSubmit.bind($submitBtn));
+            $modal.append($imageBox);
+            $modal.append($titleField);
+            $modal.append($textField);
+
+            $submitBtn.click(this.addPost);
             $shadow.click(closeParent);
         },
 
-        toggleOverlay: function () {
+        toggleOverlay: function (data) {
+            if (!$parent) {
+                this.init();
+            }
+
+            if ($parent.is(":visible")) {
+                $parent.hide();
+            } else {
+                $parent.show();
+                this.prepareShare(data);
+            }
+        },
+
+        prepareShare: function (data) {
+            if (data.title) {
+                sharedData.title = data.title;
+                $titleField.text(data.title);
+            }
+            if (data.imageSrc) {
+                sharedData.imageSrc = data.imageSrc;
+                $imageBox.attr('src', data.imageSrc);
+            }
+            if (data.text) {
+                sharedData.text = data.text;
+                $textField.text(data.text);
+            }
+        },
+
+        addPost: function () {
             debugger;
-            $parent.toggle();
+            PostStorage.push(sharedData);
         }
+
     };
 
     function closeParent() {
         $parent.hide()
     }
 
-    function onSubmit() {
-        data = getPostData();
-    }
-
-    function getPostData() {
-
-    }
 
 })(window);
 
--- app/scripts/shared/facebook.js
+++ app/scripts/shared/facebook.js
@@ -1,27 +1,52 @@
-(function () {
-    if (ChromeBuffer) {
-        init();
+(function ($) {
+    var attached = false,
+        sharedData = {
+            title: '',
+            text: '',
+            imageSrc: '',
+            url: ''
+        }
+        ;
+    check();
+
+    function check() {
+        if (ChromeBuffer) {
+            init();
+            setTimeout(check, 1500);
+        }
     }
 
     function init() {
         attachShareButton();
-
     }
 
     function attachShareButton() {
         var $shareRoots = $('.share_root'),
-            $shareBtn = $('<a href="#">SHAAAREEE!!!</a>');
+            $shareBtn;
 
         $shareRoots.each(function (index, el) {
-            debugger;
-            $(el).append($shareBtn);
-        });
+            if (!$(el).siblings('.buffer-share').length) {
+                $shareBtn = $('<span class="buffer-share"><a href="#"><span>Share!</span></a></span>');
+                $shareBtn.click(onShareBtnClick.bind($shareBtn));
 
-        $shareBtn.click(onShareBtnClick);
+                $(el).after($shareBtn);
+            }
+        });
     }
 
     function onShareBtnClick() {
-        ChromeBuffer.toggleOverlay();
+        var currentPost = $(this).parents('div[data-dedupekey]');
+        updateSharedData(currentPost);
+        ChromeBuffer.toggleOverlay(sharedData);
     }
 
-})();
+    function updateSharedData(currentPost) {
+        var closestImage = currentPost.find('img._46-i'),
+            textElem = currentPost.find('.userContent')
+            ;
+        sharedData['imageSrc'] = closestImage.length && closestImage.attr('src');
+        sharedData['text'] = textElem.text();
+        return sharedData;
+    }
+
+})($);
