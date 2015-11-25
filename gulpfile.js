450a6e252e54452cd927f3aa01b8dcfdbee97d01
Vladislav <ruspython@gmail.com>
2015-11-25 12:30:59 +0300

    Gulp added

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    paths = {
+        scripts: [],
+        less: ['./app/styles/**/*.less']
+    }
+    ;
+
+
+gulp.task('less', function () {
+    return gulp.src(paths.less)
+        .pipe(less())
+        .pipe(gulp.dest('./app/styles/'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.less, ['less']);
+});
