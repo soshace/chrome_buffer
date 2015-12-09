34a21ec5dad33cb43baa2a8a38e4b61daa264823
Vladislav <ruspython@gmail.com>
2015-12-09 16:00:39 +0300

    LinkedIn fix

--- app/scripts/embeds/linkedin.js
+++ app/scripts/embeds/linkedin.js
@@ -1,7 +1,11 @@
 (function ($) {
     var SERVICE_NAME = 'linkedin',
         attached = false,
-        postSelector = '.feed-update.channel-recommend-article, .feed-update.member-share-article',
+        postSelector = [
+            '.feed-update.channel-recommend-article',
+            '.feed-update.member-share-article',
+            '.feed-update.company-share-article'
+        ].join(', '),
         compareChildSelector = 'li .buffer-share',
         sharedData = {
             title: '',
@@ -10,24 +14,33 @@
             url: ''
         },
 
-        imageSelector =[
+        imageSelector = [
             '.content .side-article a .image-container img',
-            '.content .rich-media a .image-container img'
+            '.content .rich-media a .image-container img',
+            '.content .shared-image img',
+            '.content .video-embedded img.video-thumbnail'
         ].join(', '),
 
         titleSelector = [
             '.content .side-article .side .content a h4',
-            '.content .rich-media .side .content a h4'
-        ].join(','),
+            '.content .rich-media .side .content a h4',
+            '.content .video-embedded .embedly-info h4'
+        ].join(', '),
 
         textSelector = [
             '.content .side-article .side .content a .snippet-container',
-            '.content .rich-media .side .content a .snippet-container'
+            '.content .rich-media .side .content a .snippet-container',
+            '.content .video-embedded .embedly-info p'
         ].join(', '),
 
-        urlSelector =  [
+        urlSelector = [
             '.content .side-article .side .content a',
             '.content .rich-media .side .content a'
+        ].join(', '),
+
+        // For video links that stored in data-href
+        videoUrlSelector = [
+            '.content .video-thumbnail-container'
         ].join(', ')
         ;
     check();
@@ -74,16 +87,17 @@
     }
 
     function updateSharedData(currentPost) {
-        var $textElem     = currentPost.find(textSelector).first(),
-            $urlElem      = currentPost.find(urlSelector).first(),
-            $titleElem    = currentPost.find(titleSelector).first()
+        var $textElem = currentPost.find(textSelector).first(),
+            $urlElem = currentPost.find(urlSelector).first(),
+            $titleElem = currentPost.find(titleSelector).first()
             ;
 
         sharedData['imageSources'] = Utils.getImageSources(imageSelector, currentPost);
-        sharedData['title']    = $titleElem.length && $titleElem.text();
-        sharedData['url']      = $urlElem.length && $urlElem.attr('href');
-        sharedData['text']     = $textElem.length && $textElem.text();
-        sharedData['service']  = SERVICE_NAME;
+        sharedData['title'] = $titleElem.length && $titleElem.text();
+        sharedData['url'] = $urlElem.attr('href') || currentPost.find(videoUrlSelector).data('href');
+        sharedData['text'] = $textElem.length && $textElem.text();
+        sharedData['service'] = SERVICE_NAME;
+
         return sharedData;
     }
 
